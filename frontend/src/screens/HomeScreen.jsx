4e0a43da9f567b2257924be763f64e6cfb80f416
yinkamac <yinka.ajibabi@gmail.com>
2023-10-18 08:12:51 +0100

    Semi Colons

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,5 +1,5 @@
 import { Row, Col} from 'react-bootstrap';
-import Product from '../components/Product'
+import Product from '../components/Product';
 import products from '../products';
 
 const HomeScreen = () => {
@@ -11,10 +11,10 @@ const HomeScreen = () => {
         <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
             <Product product={product} />
         </Col>
-    ))}
+    ))};
    </Row>
    </>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
