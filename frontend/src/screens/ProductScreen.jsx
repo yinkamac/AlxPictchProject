8e0a7850c812f7cbefa677446175d24134aeec54
yinkamac <yinka.ajibabi@gmail.com>
2023-10-25 09:53:05 +0100

    Fetching Files from Database

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,14 +1,25 @@
-import { useParams } from "react-router-dom";
-import products from "../products";
+import { useState, useEffect} from 'react';
+import {useParams } from "react-router-dom";
 import {Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap';
 import Rating from "../components/Rating";
+import axios from 'axios';
+
 
 const ProductScreen = () => {
-    const {id:productId} = useParams();
-    const product = products.find((p) => p._id === productId);
-    console.log(product);
+    const [product, setProduct] = useState({});
 
+
+    const { id:productId } = useParams();
+
+    useEffect (() => {
+        const fetchProduct =async () => {
+            const { data } = await axios.get(`/api/products/${productId}`);
+            setProduct(data);
+        }
+
+        fetchProduct();
+    }, [productId]);
   return (
    <>
    <Link className='btn btn-light my-3' to='/'>
