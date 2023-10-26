f5c64bd4c85557499645f4cc3d836393a1a0237d
yinkamac <yinka.ajibabi@gmail.com>
2023-10-26 19:41:53 +0100

    Getting Products from the Database

--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,18 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js'
 const port = process.env.PORT || 5000;
 
 connectDB(); // Connect to MongoDB
 
-const app =express();
+const app = express();
 
 app.get('/', (req, res) => {
-    res.send( 'Api is running....')
+    res.send( 'Api is running....');
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req,res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
