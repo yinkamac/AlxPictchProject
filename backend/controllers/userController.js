e7a1be0155fb076809169b88a9592939e9fa0f7f
yinkamac <yinka.ajibabi@gmail.com>
2023-10-31 11:38:19 +0100

    Autherization middlewaree & Endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
 import asyncHandler from "../middleware/asyncHandler.js";
-import User from "../models/UserModel.js";
+import User from "../models/userModel.js";
 import  jwt  from 'jsonwebtoken'
 
 // @desc Auth user & get token
