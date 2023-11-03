cee0d70e270d724f09acea46e5d530104a54818e
yinkamac <yinka.ajibabi@gmail.com>
2023-11-03 16:24:18 +0100

    Checkout Steps components

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -21,7 +21,7 @@ const LoginScreen = () => {
 
   const { search } = useLocation();
   const sp = new URLSearchParams(search);
-  const redirect = sp.get('redirect') || '';
+  const redirect = sp.get('redirect') || '/';
 
   useEffect(() => {
     if (userInfo) {
