c58a3201a1fac31710e026b7703366adbd91f61b
yinkamac <yinka.ajibabi@gmail.com>
2023-11-03 11:52:01 +0100

    Shipping Screen For Users

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -3,11 +3,9 @@ import { updateCart } from "../utils/cartUtils";
 
 const initialState = localStorage.getItem("cart") 
 ? JSON.parse(localStorage.getItem("cart")) 
-: {cartItems : []};
+: {cartItems: [], shippingAddress: {}, paymentMethod: 'PayPal'};
 
-const addDecimals = (num) => {
-    return (Math.round(num * 100) /100).toFixed(2);
-}
+
 
 const cartSlice = createSlice({
     name: "cart",
@@ -29,10 +27,14 @@ const cartSlice = createSlice({
             state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
 
             return updateCart(state);
+        },
+        saveShippingAddress: (state,action) => {
+            state.shippingAddress = action.payload;
+            return updateCart(state);
         }
     },
 });
 
-export const { addToCart, removeFromCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, saveShippingAddress } = cartSlice.actions;
 
 export default cartSlice.reducer;
