396db6ff90fa70d13d557b865d19bf1769c784fa
yinkamac <yinka.ajibabi@gmail.com>
2023-11-05 11:40:24 +0100

    User Profile & Update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -24,6 +24,7 @@ import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -33,12 +34,14 @@ const router = createBrowserRouter(
       <Route path='/cart' element={<CartScreen />} />
       <Route path='/login' element={<LoginScreen />} />
       <Route path='/register' element={<RegisterScreen />} />
+      
 
       <Route path='' element={<PrivateRoute />}>
       <Route path='/shipping' element={<ShippingScreen />} /> 
       <Route path='/payment' element={<PaymentScreen />} /> 
       <Route path='/placeorder' element={<PlaceOrderScreen />} />
-      <Route path='/order/:id' element={<OrderScreen />} /> 
+      <Route path='/order/:id' element={<OrderScreen />} />
+      <Route path='/profile' element={<ProfileScreen />} /> 
       </Route>
     </Route>
   )
