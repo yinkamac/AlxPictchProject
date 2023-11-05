c56c95e24a25535ce164275d74fe7287de60b78b
yinkamac <yinka.ajibabi@gmail.com>
2023-11-05 11:09:14 +0100

    PayPal Button

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -10,7 +10,7 @@ import {
     Card,
 } from 'react-bootstrap';
 import { toast } from 'react-toastify';
-import { UseSelector, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js'
 import Message from '../components/Message';
 import Loader from  '../components/Loader';
@@ -42,7 +42,7 @@ const OrderScreen = () => {
                     type: 'resetOptions',
                     value: {
                         'client-id': paypal.clientId,
-                        currency: 'NGN',
+                        currency: 'USD',
                     },
                 });
                 paypalDispatch({ type: 'setLoadingStatus', value: 'pending'});
@@ -54,6 +54,42 @@ const OrderScreen = () => {
             }
         }
     }, [order, paypal, paypalDispatch, loadingPayPal, errorPayPal]);
+
+    function onApprove(data, actions) {
+        return actions.order.capture().then(async function (details) {
+            try {
+                await payOrder({ orderId, details });
+                refetch();
+                toast.success('Payment successful');
+            } catch (err) {
+                toast.error(err?.data?.message || err.message);
+            }
+        });
+    }
+
+    async function onApproveTest() {
+        await payOrder({ orderId, details: { payer: {} } });
+        refetch();
+        toast.success('Payment successful');
+    }
+
+    function onError(err) {
+        toast.error(err.message);
+    }
+    function createOrder(data, actions) {
+        return actions.order.create({
+            purchase_units: [
+                {
+                    amount: {
+                        value: order.totalPrice,
+                    },
+                },
+            ],
+        })
+        .then((orderId) => {
+            return orderId;
+        });
+    }
 
     return isLoading ? (
     <Loader />
@@ -96,7 +132,7 @@ const OrderScreen = () => {
                     </p>
                     {order.isPaid ? (
                         <Message variant='success'>
-                            Delivered on {order.paidAt}
+                            Paid on {order.paidAt}
                         </Message>
                     ) : (
                         <Message variant='danger'>Not Paid</Message>
@@ -153,7 +189,33 @@ const OrderScreen = () => {
                                 <Col>₦{order.totalPrice}</Col>
                             </Row>
                         </ListGroup.Item>
-                        {/* PAY ORDER PLACEHOLDER */}
+
+                        {!order.isPaid && (
+                            <ListGroup.Item>
+                                {loadingPay && <Loader />}
+
+                                {isPending ?  ( <Loader /> 
+                                ) : (
+                                    <div>
+                                        <Button
+                                            onClick={onApproveTest}
+                                            style={{ marginBottom: '10px' }}
+                                            >
+                                                Test Pay Order
+                                        </Button>
+                                        <div>
+                                            <PayPalButtons
+                                            createOrder={createOrder}
+                                            onApprove={onApprove}
+                                            onError={onError}
+                                            >
+
+                                            </PayPalButtons>
+                                        </div>
+                                    </div>
+                                )}
+                            </ListGroup.Item>
+                        )}
                          {/* MARK AS DELIVERED PLACEHOLDER */}
                     </ListGroup>
                 </Card>
