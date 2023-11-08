b9aeda775d0dfb372c734d1b5b00265f3dfbf93b
yinkamac <olayinka.ajibabi@gmail.com>
2023-11-08 01:00:12 +0100

    Bugs & Error fix

--- frontend/src/screens/admin/ProductEditScreen.jsx
+++ frontend/src/screens/admin/ProductEditScreen.jsx
@@ -31,7 +31,7 @@ const ProductEditScreen = () => {
 
       const [updateProduct, { isLoading: loadingUpdate }] = useUpdateProductMutation();
 
-      const [uploadProductImage, { isLoading: loadingUpload }] = useUploadProductImageMutation();
+      const [uploadProductImage, ] = useUploadProductImageMutation();
 
       const navigate = useNavigate();
 
